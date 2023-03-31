8a3e9ff3351ca070f71acbe3610e9318c7c15104
mNik033 <nktps40@gmail.com>
2023-03-31 14:24:28 +0530

    redo intro activity layout

--- app/src/main/java/com/ink/lnf/activities/IntroActivity.kt
+++ app/src/main/java/com/ink/lnf/activities/IntroActivity.kt
@@ -1,32 +1,27 @@
 package com.ink.lnf.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
-import android.widget.Button
+import android.widget.TextView
 import com.ink.lnf.R
 
 class IntroActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+
         var currentUserID = BaseActivity().getCurrentUserID()
         if (currentUserID.isNotEmpty()){
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
-        val btnSignInIntroActivity = findViewById<Button>(R.id.buttonSignInIntro)
+        val btnSignInIntroActivity = findViewById<TextView>(R.id.buttonSignInIntro)
         btnSignInIntroActivity.setOnClickListener {
             startActivity(Intent(this, SignInActivity::class.java))
             finish()
         }
 
-        val btnSignUpIntroActivity = findViewById<Button>(R.id.buttonSignUpIntro)
+        val btnSignUpIntroActivity = findViewById<TextView>(R.id.buttonSignUpIntro)
         btnSignUpIntroActivity.setOnClickListener{
             startActivity(Intent(this, SignUpActivity::class.java))
             finish()
